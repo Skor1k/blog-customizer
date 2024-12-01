132841fef8695e379ebcabfbbc956ae23f9947be
Alexei Skorik <76skor1k@gmail.com>
2024-12-01 21:11:06 +0300

    Commit-1-add-App

---
+++ src/components/app/App.tsx
@@ -0,0 +1,27 @@
+import { CSSProperties } from 'react';
+import clsx from 'clsx';
+
+import { Article } from '../article';
+import { ArticleParamsForm } from '../article-params-form';
+import { defaultArticleState } from '../../constants/articleProps';
+
+import styles from '../../styles/index.module.scss';
+
+export const App = () => {
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': defaultArticleState.fontFamilyOption.value,
+					'--font-size': defaultArticleState.fontSizeOption.value,
+					'--font-color': defaultArticleState.fontColor.value,
+					'--container-width': defaultArticleState.contentWidth.value,
+					'--bg-color': defaultArticleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm />
+			<Article />
+		</main>
+	);
+};
